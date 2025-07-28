878eb2aa20254e967c528c165e5450cb69605dee
Pedro Militão <pepmilitao@gmail.com>
2025-07-27 22:04:40 -0300

    App transformado em container docker

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
 db = SQLAlchemy()
 
 # Configuração de conexão
 # TODO: Esconder configurações de usuário e senha
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pep:123@localhost:5432/postgres'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db.init_app(app)
